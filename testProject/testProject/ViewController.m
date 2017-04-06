9b725eb5fcc49d9b5ecad1c23495c41e8387419b
zhc776 <584340617@qq.com>
2017-04-06 10:56:14 +0800

    添加视图view

--- testProject/testProject/ViewController.m
+++ testProject/testProject/ViewController.m
@@ -19,6 +19,12 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     // 添加一条注释
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    view.backgroundColor = [UIColor darkGrayColor];
+    [self.view addSubview:view];
+    
+    
 }
 
 
